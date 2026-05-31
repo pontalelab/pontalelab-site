424c3612bc61f37cf2bfd426ff77fe9454b2bef5
pontalelab <pontalelab@gmail.com>
2026-06-01 01:32:25 +0900

    Delete mushimushi directory

--- mushimushi/portal/games/mazemaze/storage/StoragePort.js
+++
@@ -1,5 +0,0 @@
-function StoragePort() {}
-StoragePort.prototype.saveData   = function() { throw new Error('saveData not implemented'); };
-StoragePort.prototype.loadData   = function() { throw new Error('loadData not implemented'); };
-StoragePort.prototype.removeData = function() { throw new Error('removeData not implemented'); };
-StoragePort.prototype.clear      = function() { throw new Error('clear not implemented'); };
--- mushimushi/src/constants/run.ts
+++
@@ -1,2 +0,0 @@
-export const RUN_STAGES = ['grass_day', 'forest_evening', 'night_field'] as const
-export const CAPTURES_PER_STAGE = 3
